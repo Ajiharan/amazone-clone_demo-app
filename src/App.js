e814b38e79015e756b0386ae56792074759ca020
Ajiharan <ajiharan@unicomtechnology.co.uk>
2020-10-22 11:25:41 +0530

    add home banner

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
-import React,{useEffect} from 'react';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import './App.css';
-import Header from './Header';
-import Home from './home/Home';
-import CheckOut from './checkout/CheckOut';
-import Login from './user/Login';
-import {useStateValue} from './redux/StateProvider';
-import {auth} from './firebase/Firebase';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Home from "./home/Home";
+import CheckOut from "./checkout/CheckOut";
+import Login from "./user/Login";
+import { useStateValue } from "./redux/StateProvider";
+import { auth } from "./firebase/Firebase";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
 
-  const[{user},dispatch]=useStateValue();
-  useEffect(()=>{
-   const unsubscribe= auth.onAuthStateChanged((authUser)=>{
-      if(authUser){
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      console.log("AuthUser", authUser);
+      if (authUser) {
         dispatch({
-          type:'SET_USER',
-          user:authUser
-        })
-      }else{
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
         dispatch({
-          type:'SET_USER',
-          user:null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
     });
-    return ()=>{
+    return () => {
       unsubscribe();
-    }
-  },[]);
-  console.log('User is',user)
+    };
+  }, []);
+  console.log("User is", user);
   return (
     <Router>
-      <div className="App">   
+      <div className="App">
         <Switch>
           <Route exact path="/">
-            <Header/>
-            <Home/>
+            <Header />
+            <Home />
           </Route>
           <Route exact path="/checkout">
-            <Header/>
-            <CheckOut/>
+            <Header />
+            <CheckOut />
           </Route>
           <Route path="/login">
-            <Login/>
+            <Login />
           </Route>
         </Switch>
       </div>
     </Router>
-    
   );
 }
 
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,42 +1,46 @@
-import Cookie from 'js-cookie';
+import Cookie from "js-cookie";
 
-const basketItems=Cookie.getJSON("CartItems") || [];
-export const initialState={
-    basket:basketItems,
-    user:{}
+const basketItems = Cookie.getJSON("CartItems") || [];
+export const initialState = {
+  basket: basketItems,
+  user: {},
 };
 
-export const getBasketTotal=(basket)=>{
-    return basket?.reduce((acc,i)=>acc+i.price,0);
-}
+export const getBasketTotal = (basket) => {
+  return basket?.reduce((acc, i) => acc + i.price, 0);
+};
 
-export const reducer=(state=initialState,action)=>{
-    console.log("state",state);
-    switch(action.type){
+export const reducer = (state = initialState, action) => {
+  console.log("state", state);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "ADD_TO_BASKET":
+      const product = state.basket.find((e) => e.id === action.item.id);
 
-        case 'SET_USER':
-            return{
-                ...state,
-                user:action.user
-            }
-        case 'ADD_TO_BASKET':
-            const product=state.basket.find(e=>e.id===action.item.id);
-            if(product){
-                return{
-                    ...state,basket:[...state.basket.map(e=>e.id===product.id?action.item:e)]
-                }
-            }else{
-                return {
-                    ...state,
-                    basket:[...state.basket,action.item]
-                };
-            }
-              
-        case 'REMOVE_FROM_BASKET':
-            let currentState=state.basket.filter(e=>e.id !==action.id);
-            return {...state,basket:[...currentState]};
-           
-        default:
-            return state;
-    }
-}
+      if (product) {
+        return {
+          ...state,
+          basket: [
+            ...state.basket.map((e) => (e.id === product.id ? action.item : e)),
+          ],
+        };
+      } else {
+        return {
+          ...state,
+          basket: [...state.basket, action.item],
+        };
+      }
+
+    case "REMOVE_FROM_BASKET":
+      let currentState = state.basket.filter((e) => e.id !== action.id);
+      Cookie.set("CartItems", JSON.stringify(currentState));
+      return { ...state, basket: [...currentState] };
+
+    default:
+      return state;
+  }
+};
